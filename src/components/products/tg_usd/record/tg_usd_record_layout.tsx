1772db0a8706bb76de770732e0f4df9f425d3b87
yann-j-nomiks <yann.j@zohomail.com>
2025-01-08 06:52:04 +0100

    feat: tgUsd record graphs

--- src/components/products/tg_usd/record/tg_usd_record_layout.tsx
+++ src/components/products/tg_usd/record/tg_usd_record_layout.tsx
@@ -5,12 +5,13 @@ import { TgUsdRecordProvider } from "./tg_usd_record_provider"
 
 import TgUsdRecordPageHader from "./tg_usd_record_page_header"
 import { useNavigationContext } from "../../product_nav/navigation_context"
-import TgUsdLoanDetail from "./tg_usd_load_detail"
+import TgUsdLoanDetail from "./tg_usd_loan_detail"
 import Divider from "@/components/design_system/structure/divider"
 import Panel from "@/components/design_system/structure/panel"
 import ButtonTab from "@/components/design_system/inputs/button_tab"
 import PanelRaw from "@/components/design_system/structure/panel_raw"
 import TgUsdCollateralPrice from "./tg_usd_collateral_price"
+import TgUsdMarketInfo from "./tg_usd_market_info"
 
 type TgUsdRecordLayoutProps = React.ButtonHTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode
@@ -37,26 +38,30 @@ export default function TgUsdRecordLayout({ children, ...props }: TgUsdRecordLay
         <Divider />
         <TgUsdLoanDetail />
       </div>
-
-      <div className="flex gap-4 max-xl:flex-col">
-        <div className="xl:w-1/2">
-          <Panel className="h-full">
-            <div className="flex justify-between">
-              <div className="flex gap-2">
-                <ButtonTab active={currentFeature === "deposit"} label={"Deposit"} onClick={() => onTabClick("deposit")} />
-                <ButtonTab active={currentFeature === "borrow"} label={"Borrow"} onClick={() => onTabClick("borrow")} />
-                <ButtonTab active={currentFeature === "repay"} label={"Repay"} onClick={() => onTabClick("repay")} />
-                <ButtonTab active={currentFeature === "withdraw"} label={"Withdraw"} onClick={() => onTabClick("withdraw")} />
-                <ButtonTab active={currentFeature === "liquidate"} label={"Luquidate"} onClick={() => onTabClick("liquidate")} />
+      <div className="mt-2 flex flex-col gap-4">
+        <div className="flex gap-4 max-xl:flex-col">
+          <div className="xl:w-1/2">
+            <Panel className="h-full">
+              <div className="flex justify-between">
+                <div className="flex gap-2">
+                  <ButtonTab active={currentFeature === "deposit"} label={"Deposit"} onClick={() => onTabClick("deposit")} />
+                  <ButtonTab active={currentFeature === "borrow"} label={"Borrow"} onClick={() => onTabClick("borrow")} />
+                  <ButtonTab active={currentFeature === "repay"} label={"Repay"} onClick={() => onTabClick("repay")} />
+                  <ButtonTab active={currentFeature === "withdraw"} label={"Withdraw"} onClick={() => onTabClick("withdraw")} />
+                  <ButtonTab active={currentFeature === "liquidate"} label={"Luquidate"} onClick={() => onTabClick("liquidate")} />
+                </div>
               </div>
-            </div>
-            <div className="mt-5">{children}</div>
-          </Panel>
+              <div className="mt-5">{children}</div>
+            </Panel>
+          </div>
+          <div className="flex h-full w-full flex-col gap-2 xl:w-1/2">
+            <PanelRaw className="p-4">
+              <TgUsdCollateralPrice />
+            </PanelRaw>
+          </div>
         </div>
-        <div className="flex h-full w-full flex-col gap-2 xl:w-1/2">
-          <PanelRaw className="p-4">
-            <TgUsdCollateralPrice />
-          </PanelRaw>
+        <div>
+          <TgUsdMarketInfo />
         </div>
       </div>
     </TgUsdRecordProvider>

cd5263e1d73d6539154153521a4f52aa7a4a3326
Vincent <vincent.granie@2018.icam.fr>
2025-12-17 12:02:59 +0100

    add token info link

--- src/components/products/usg/record/header/components/market_details_info.tsx
+++ src/components/products/usg/record/header/components/market_details_info.tsx
@@ -5,10 +5,12 @@ import { useMemo, useState } from "react"
 import tokenInfos from "@/data/tokenInfos.json"
 import { MarketDetailData } from "../../../usg_type"
 import TokenImage from "@/components/design_system/structure/token_image"
+import { IconArrow } from "@/components/icons"
 
 type TokenInfo = {
   info: string
   risk: string
+  link: string
 }
 
 type TokenInfosMap = Record<string, TokenInfo>
@@ -31,31 +33,58 @@ export const MarketDetailsInfos = ({ marketData }: MarketDetailsInfosProps) => {
     return info ?? { info: "No information available", risk: "—" }
   }, [selectedToken])
 
+  const onClickTokenLink = (token: ExistingAsset) => {
+    const infoToDisplay = typedTokenInfos[token]
+    window.open(infoToDisplay?.link, "_blank")
+  }
+
   return (
     <div className="mt-4 hidden h-24 items-center rounded-[10px] bg-overlay-panel py-2 backdrop-blur-[60px] md:flex">
       <div className="flex min-h-20 w-2/12 flex-col items-center justify-center border-r border-[#3F3F3F] px-8">
         <div className="flex flex-col items-center justify-between gap-2">
-          <button
-            onClick={() => setSelectedToken(tokenA)}
-            className={`mt-2 flex min-w-20 items-center justify-between gap-2 rounded-full border border-white px-2 py-1 text-xs transition-all ${
-              selectedToken === tokenA ? "border-opacity-100 bg-white text-black" : "border-opacity-20 text-white hover:border-opacity-100"
-            }`}
-          >
-            <TokenImage token={tokenA as ExistingAsset} size={8} className="w-4" />
-            {tokenA}
-          </button>
-
-          {tokenB && tokenB !== "" && (
+          <div className="mt-2 flex items-center justify-between gap-2">
             <button
-              onClick={() => setSelectedToken(tokenB)}
-              className={`mt-1 flex min-w-20 items-center justify-between rounded-full border border-white px-2 py-1 text-xs transition-all ${
-                selectedToken === tokenB ? "border-opacity-100 bg-white text-black" : "border-opacity-20 text-white hover:border-opacity-100"
+              onClick={() => setSelectedToken(tokenA)}
+              className={`flex min-w-20 items-center justify-between gap-2 rounded-full border border-white px-2 py-0.5 text-xs transition-all ${
+                selectedToken === tokenA ? "border-opacity-100 bg-white text-black" : "border-opacity-20 text-white hover:border-opacity-100"
               }`}
             >
-              <TokenImage token={tokenB as ExistingAsset} size={8} className="w-4" />
-              {tokenB}
+              <TokenImage token={tokenA as ExistingAsset} size={8} className="w-4" />
+              {tokenA}
             </button>
-          )}
+
+            <div
+              onClick={() => onClickTokenLink(tokenA)}
+              className="relative flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-full border border-subtitle hover:border-white"
+            >
+              <div className="absolute flex items-center justify-center">
+                <IconArrow className="w-4 fill-white"></IconArrow>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-between gap-2">
+            {tokenB && tokenB !== "" && (
+              <>
+                <button
+                  onClick={() => setSelectedToken(tokenB)}
+                  className={`mt-1 flex min-w-20 items-center justify-between rounded-full border border-white px-2 py-0.5 text-xs transition-all ${
+                    selectedToken === tokenB ? "border-opacity-100 bg-white text-black" : "border-opacity-20 text-white hover:border-opacity-100"
+                  }`}
+                >
+                  <TokenImage token={tokenB as ExistingAsset} size={8} className="w-4" />
+                  {tokenB}
+                </button>
+                <div
+                  onClick={() => onClickTokenLink(tokenB as ExistingAsset)}
+                  className="relative flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-full border border-subtitle hover:border-white"
+                >
+                  <div className="absolute flex items-center justify-center">
+                    <IconArrow className="w-4 fill-white"></IconArrow>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
 

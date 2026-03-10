e890425b11844506f62f929438c53acef5d7f5e7
Vincent <vincent.granie@2018.icam.fr>
2026-03-10 17:36:31 +1300

    push for pipeline

--- src/components/products/wallet/wallet_connexion_controller.tsx
+++ src/components/products/wallet/wallet_connexion_controller.tsx
@@ -1,15 +1,12 @@
-import { Abi, Address, Hex, zeroAddress } from "viem"
 import { USG_CONTRACT } from "../usg/usg_repository"
+import { Abi, Address, Hex, zeroAddress } from "viem"
 import { executeChainViewUnique } from "@/services/service_rpc"
 import GetBalancesWithVsTan from "../../../abi/USG/GetBalancesWithVsTan.json"
-// import { VSTAN_CONTRACT } from "../vs_tan/rs_tan_repository"
 
 export const getUserBalances = async (currentAddress: Address) => {
   const tokenMap: Record<string, string> = {
     [USG_CONTRACT.USG.toLowerCase()]: "USG",
     [USG_CONTRACT.SUSG.toLowerCase()]: "sUSG",
-    // [VSTAN_CONTRACT.TAN.toLowerCase()]: "TAN",
-    // [VSTAN_CONTRACT.STAN.toLowerCase()]: "sTAN",
   }
 
   const addresses = Object.keys(tokenMap)

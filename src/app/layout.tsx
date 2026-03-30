d348a3072d70c14872f776385132ab0b74088484
Ya-Je <95443605+Ya-Je@users.noreply.github.com>
2026-03-30 17:57:24 +0200

    Comment out fixed background div in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={gilroy.variable} suppressHydrationWarning>
       <body className="dark relative bg-dark font-gilroy antialiased" id="body" suppressHydrationWarning>
-        {* <div className="fixed inset-0 -z-30 bg-body"></div> *}
+        {/* <div className="fixed inset-0 -z-30 bg-body"></div> */}
         <main className="overflow-y-overlay mx-auto min-h-[80vh] w-full">{children}</main>
       </body>
     </html>

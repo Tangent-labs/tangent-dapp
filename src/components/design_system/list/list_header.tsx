54fd06544bdc23ca0c0e64e4a897187904cba67a
yaje <yann.j@zohomail.com>
2026-04-27 11:13:25 +0200

    format

--- src/components/design_system/list/list_header.tsx
+++ src/components/design_system/list/list_header.tsx
@@ -76,17 +76,19 @@ export const ListHeader = ({ headers, activeSort, onSort, rowDisposition: Custom
               className={cn(headers[1]?.className || "", "flex w-full items-center justify-center")}
             />
           )}
-          {headers?.slice(2)?.map((header) => (
-            <HeaderDisplay
-              key={header.key}
-              label={header.label}
-              sort={(activeSort?.key == header.key && activeSort?.direction) || "none"}
-              field={header.key}
-              onSort={!!header.sort ? onSort : undefined}
-              indicator={header.indicator}
-              className={cn(header.className || "", "flex w-full flex-1 items-center justify-center")}
-            />
-          ))}
+          {headers
+            ?.slice(2)
+            ?.map((header) => (
+              <HeaderDisplay
+                key={header.key}
+                label={header.label}
+                sort={(activeSort?.key == header.key && activeSort?.direction) || "none"}
+                field={header.key}
+                onSort={!!header.sort ? onSort : undefined}
+                indicator={header.indicator}
+                className={cn(header.className || "", "flex w-full flex-1 items-center justify-center")}
+              />
+            ))}
         </CustomRowDisposition>
       </div>
 

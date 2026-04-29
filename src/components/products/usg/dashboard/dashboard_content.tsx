66f4d648acc2d64fffb72efd567a18bb98a426de
Vincent <vincent.granie@2018.icam.fr>
2026-04-29 16:21:03 +1200

    quick css fix

--- src/components/products/usg/dashboard/dashboard_content.tsx
+++ src/components/products/usg/dashboard/dashboard_content.tsx
@@ -30,17 +30,15 @@ export const USGDashboardContent = () => {
   return (
     <div className="flex w-full flex-col items-start justify-start gap-5">
       {/* POINTS CAMPAIGN COMPONENT */}
-      <div className="flex h-full w-full flex-col items-start justify-start gap-8 rounded-[10px] bg-overlay-panel backdrop-blur-[60px]">
-        <ReliefCard className="w-full">
-          <div
-            style={{ fontSize: "20px", lineHeight: "20px" }}
-            className="flex h-16 w-full items-center justify-start rounded-[10px] bg-[url('/medias/fulltan.png')] bg-[size:30%] bg-[position:calc(100%)_bottom] bg-no-repeat px-6 !font-semibold italic"
-          >
-            Points campaign
-            <div className="ml-6 flex items-center justify-center rounded-[10px] bg-tonic px-6 py-0.5 font-semibold not-italic text-black">Live</div>
-          </div>
-        </ReliefCard>
-      </div>
+      <ReliefCard className="w-full">
+        <div
+          style={{ fontSize: "20px", lineHeight: "20px" }}
+          className="flex h-16 w-full items-center justify-start rounded-[10px] bg-[url('/medias/fulltan.png')] bg-[size:30%] bg-[position:calc(100%)_bottom] bg-no-repeat px-6 !font-semibold italic"
+        >
+          Points campaign
+          <div className="ml-6 flex items-center justify-center rounded-[10px] bg-tonic px-6 py-0.5 font-semibold not-italic text-black">Live</div>
+        </div>
+      </ReliefCard>
 
       <div className="flex w-full flex-col gap-5 lg:flex-row">
         <GraphTokenPrice

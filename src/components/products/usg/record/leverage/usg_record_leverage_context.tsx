0dc09c7173bab404d02a1d23f3ee15c910f37f40
Trottoir <maxime.ranciere@2018.icam.fr>
2026-06-03 13:35:14 +0200

    Add slippage for simple leverage

--- src/components/products/usg/record/leverage/usg_record_leverage_context.tsx
+++ src/components/products/usg/record/leverage/usg_record_leverage_context.tsx
@@ -507,7 +507,15 @@ export const USGLeverageProvider = ({ children }: USGLeverageContextProps) => {
       const isReceiptIn = marketData?.constants?.receipt.toLowerCase() === depositAssetInfo?.address.toLowerCase()
 
       await toastTx(
-        doMarketLeverage(marketInfo?.marketAddress, walletClient, depositWeiValue || 0n, borrowWeiValue, leveragedCollateralQuote, isReceiptIn, leverageData!),
+        doMarketLeverage(
+          marketInfo?.marketAddress,
+          walletClient,
+          depositWeiValue || 0n,
+          borrowWeiValue,
+          computedMinAmountOut(leveragedCollateralQuote, slippage),
+          isReceiptIn,
+          leverageData!
+        ),
         {
           pending: { type: "Pending Transaction", content: "Blockchain transaction in progress..." },
           success: () => ({

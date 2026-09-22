acb029af96d9f43a8b11de1d0a05e55d7347e24e
Trottoir <31350013+Trottoir@users.noreply.github.com>
2026-09-22 13:48:05 +0200

    Preprod (#782)
    * Add USG logo for CMC (#775) (#776)
    
    Fix Harvest special rewards
    
    * Add daily revenues graph (#778)
    
    * Add fixed interest rate recognition + FIR badge (#781)

--- src/components/design_system/list/collat_emission_label.tsx
+++ src/components/design_system/list/collat_emission_label.tsx
@@ -3,24 +3,33 @@ import Link from "next/link"
 
 type CollateralEmissionLabelProps = {
   isHEC: boolean
+  isFixedRate?: boolean
 }
 
-export const CollateralEmissionLabel = ({ isHEC }: CollateralEmissionLabelProps) => {
+const DOCS = {
+  FIR: "https://docs.tangent.finance/docs/usg/Markets/fir_markets",
+  HEC: "https://docs.tangent.finance/docs/usg/Markets/hec_markets#hec-markets",
+  LEC: "https://docs.tangent.finance/docs/usg/Markets/lec_markets",
+}
+
+export const CollateralEmissionLabel = ({ isHEC, isFixedRate }: CollateralEmissionLabelProps) => {
+  const type = isFixedRate ? "FIR" : isHEC ? "HEC" : "LEC"
+
   return (
     <div className="flex items-center justify-center text-white transition duration-200">
       <HoverCard openDelay={100} closeDelay={100}>
         <HoverCardTrigger asChild>
           <span className="flex h-6 items-center justify-center rounded-full bg-overlay-panel px-2 py-0.5 text-xs backdrop-blur-[60px] hover:bg-white/10">
-            {isHEC ? "HEC" : "LEC"}
+            {type}
           </span>
         </HoverCardTrigger>
         <HoverCardContent side="top" align="center" className="z-[9999] p-2 text-sm">
-          This market is an {isHEC ? "HEC" : "LEC"} market. Learn more about {isHEC ? "HEC" : "LEC"} in the
+          This market is an {type} market. Learn more about {type} in the
           <Link
             onClick={(e) => e.stopPropagation()}
             onMouseDown={(e) => e.stopPropagation()}
             className="ml-1 cursor-pointer underline hover:text-white/40"
-            href={isHEC ? "https://docs.tangent.finance/docs/usg/Markets/hec_markets#hec-markets" : "https://docs.tangent.finance/docs/usg/Markets/lec_markets"}
+            href={DOCS[type]}
             target="_blank"
             rel="noopener noreferrer"
           >
